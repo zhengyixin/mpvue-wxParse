d1a5941c6d2684533a9f9a244df40614bdbe2f5e
F-loat <945852046@qq.com>
2018-03-15 20:47:40 +0800

    移除 markdown 支持

--- src/wxParse.js
+++ src/wxParse.js
@@ -12,33 +12,15 @@
 /**
  * utils函数引入
  **/
-import showdown from './libs/showdown';
 import HtmlToJson from './libs/html2json';
-/**
- * 配置及公有属性
- **/
-var realWindowWidth = 0;
-var realWindowHeight = 0;
-wx.getSystemInfo({
-  success: function(res) {
-    realWindowWidth = res.windowWidth;
-    realWindowHeight = res.windowHeight;
-  }
-});
+
 /**
  * 主函数入口区
  **/
-export const wxParse = (type, data, imagePadding, DEBUG) => {
+export const wxParse = (data, imagePadding, DEBUG) => {
   var transData = {}; //存放转化后的数据
-  if (type == 'html') {
     transData = HtmlToJson.html2json(data, 'content');
     if (DEBUG) console.log(JSON.stringify(transData, ' ', ' '));
-  } else if (type == 'md' || type == 'markdown') {
-    var converter = new showdown.Converter();
-    var html = converter.makeHtml(data);
-    transData = HtmlToJson.html2json(html, 'content');
-    if (DEBUG) console.log(JSON.stringify(transData, ' ', ' '));
-  }
   transData.view = {};
   transData.view.imagePadding = 0;
   if (typeof imagePadding != 'undefined') {
@@ -47,26 +29,10 @@ export const wxParse = (type, data, imagePadding, DEBUG) => {
   return transData;
 };
 
-export const wxParseTemArray = (temArrayName, bindNameReg, total, that) => {
-  var array = [];
-  var temData = that.data;
-  var obj = null;
-  for (var i = 0; i < total; i++) {
-    var simArr = temData[bindNameReg + i].nodes;
-    array.push(simArr);
-  }
-
-  temArrayName = temArrayName || 'wxParseTemArray';
-  obj = JSON.parse('{"' + temArrayName + '":""}');
-  obj[temArrayName] = array;
-  that.setData(obj);
-};
-
 /**
  * 配置emojis
  *
  */
-
 export const emojisInit = (reg = '', baseSrc = '/wxParse/emojis/', emojis) => {
   HtmlToJson.emojisInit(reg, baseSrc, emojis);
 };
