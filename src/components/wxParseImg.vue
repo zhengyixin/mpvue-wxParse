753e5f1eb59eb19e929211bb950756f64bfdef48
F-loat <945852046@qq.com>
2018-04-29 23:32:41 +0800

    较小图片不进入预览队列

--- src/components/wxParseImg.vue
+++ src/components/wxParseImg.vue
@@ -3,7 +3,7 @@
     :mode="node.image.mode"
     :lazy-load="node.image.lazyLoad"
     :class="node.classStr"
-    :style="fitStyleStr"
+    :style="newStyleStr || node.styleStr"
     :data-src="node.attr.src"
     :src="node.attr.src"
     @tap="wxParseImgTap"
@@ -19,6 +19,7 @@ export default {
       realWindowWidth: 0,
       realWindowHeight: 0,
       newStyleStr: '',
+      preview: true
     };
   },
   props: {
@@ -32,11 +33,6 @@ export default {
   mounted() {
     this.getSysWH();
   },
-  computed: {
-    fitStyleStr() {
-      return this.newStyleStr || this.node.styleStr;
-    },
-  },
   methods: {
     getSysWH() {
       // 获取当前设备屏幕宽度和高度, 写在这里只是为了方便调试, 最好是写到 wxParse.vue 再传入
@@ -48,6 +44,7 @@ export default {
       });
     },
     wxParseImgTap(e) {
+      if (!this.preview) return;
       const { src } = e.target.dataset;
       if (!src) return;
       wx.previewImage({
@@ -59,12 +56,6 @@ export default {
     wxParseImgLoad(e) {
       const { src } = e.target.dataset;
       if (!src) return;
-      if (typeof src !== 'undefined' && src !== '' && src !== null) {
-        this.calMoreImageInfo(e);
-      }
-    },
-    // 假循环获取计算图片视觉最佳宽高
-    calMoreImageInfo(e) {
       const { width, height } = e.mp.detail;
       const recal = this.wxAutoImageCal(width, height);
       const { imageheight, imageWidth } = recal;
@@ -77,6 +68,13 @@ export default {
       const { padding } = this.node.image;
       const windowWidth = this.realWindowWidth - (2 * padding);
       const results = {};
+
+      if (originalWidth < 100) {
+        const { urls } = this.node.image;
+        const { src } = this.node.attr;
+        this.preview = false;
+        urls.splice(urls.indexOf(src), 1);
+      }
 
       // 判断按照那种方式进行缩放
       if (originalWidth > windowWidth) {
