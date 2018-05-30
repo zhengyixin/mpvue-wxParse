7d6989620719fd8308df1ecf0a6fec16d2ecb3d3
F-loat <945852046@qq.com>
2018-05-30 16:47:47 +0800

    feat: 优化空格字符解析

--- src/libs/wxDiscode.js
+++ src/libs/wxDiscode.js
@@ -99,16 +99,13 @@ function strGreeceDiscode(str) {
   return str;
 }
 
-//
-
 function strcharacterDiscode(str) {
   // 加入常用解析
   str = str.replace(/&nbsp;/g, ' ');
+  str = str.replace(/&ensp;/g, ' ');
+  str = str.replace(/&emsp;/g, '　');
   str = str.replace(/&quot;/g, "'");
   str = str.replace(/&amp;/g, '&');
-  // str = str.replace(/&lt;/g, '‹');
-  // str = str.replace(/&gt;/g, '›');
-
   str = str.replace(/&lt;/g, '<');
   str = str.replace(/&gt;/g, '>');
   str = str.replace(/&#8226;/g, '•');
